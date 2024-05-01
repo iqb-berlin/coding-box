daa612cd36d18a08139de7bc2ae3db1516f05a5e
jurei733 <67505990+jurei733@users.noreply.github.com>
2024-05-01 22:47:05 +0200

    Add login component

---
+++ apps/frontend/src/app/components/login/login.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule, UntypedFormGroup } from '@angular/forms';
+import { NgIf } from '@angular/common';
+import { TranslateModule, TranslatePipe } from '@ngx-translate/core';
+import { KeycloakService } from 'keycloak-angular';
+import { UnitPlayerComponent } from '../unit-player/unit-player.component';
+import { UploadUnitButtonComponent } from '../upload-unit-button/upload-unit-button.component';
+
+@Component({
+  selector: 'coding-box-login',
+  standalone: true,
+  // eslint-disable-next-line max-len
+  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, ReactiveFormsModule, NgIf, TranslateModule, UnitPlayerComponent,UploadUnitButtonComponent],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  constructor(private keycloakService: KeycloakService) {}
+
+  logout() {
+    this.keycloakService.logout();
+    console.log('login');
+  }
+}
