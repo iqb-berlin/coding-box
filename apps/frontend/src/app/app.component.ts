b45336e8386f874fc8598cb77c29572735beb68b
jurei733 <67505990+jurei733@users.noreply.github.com>
2024-05-03 17:20:12 +0200

    Add keycloak auth

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -1,24 +1,67 @@
-import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { APP_INITIALIZER, Component, OnInit } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { LoginComponent } from './components/login/login.component';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
+import { TranslateModule } from '@ngx-translate/core';
+import { MatTooltip } from '@angular/material/tooltip';
+import { KeycloakService } from 'keycloak-angular';
+import { MatButton } from '@angular/material/button';
 import { AppService } from './services/app.service';
+import { AuthService } from './auth/service/auth.service';
+import { initializer } from './auth/keycloak-initializer';
+import { CreateUserDto } from '../../api-dto/user/create-user-dto';
+import { BackendService } from './services/backend.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatSlideToggleModule, LoginComponent],
+  imports: [RouterOutlet, MatSlideToggleModule, MatProgressSpinner, RouterLink, TranslateModule, MatTooltip, MatButton],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  providers: [AuthService, {
+    provide: APP_INITIALIZER,
+    useFactory: initializer,
+    multi: true,
+    deps: [KeycloakService]
+  }]
 })
 export class AppComponent implements OnInit {
   constructor(
-    public appService: AppService) {}
+    public appService: AppService, public authService:AuthService, public keycloakService:KeycloakService, public backendService:BackendService) {}
 
+  loggedIn = false;
   title = 'coding-box';
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
+    if (!this.keycloakService.isLoggedIn()) {
+      await this.authService.login();
+      this.loggedIn = false;
+    } else {
+      this.loggedIn = true;
+      const loggedUser = this.authService.getLoggedUser();
+      console.log('loggedUser', loggedUser);
+      const userProfile = await this.authService.loadUserProfile();
+      console.log('userProfile', userProfile);
+      if (userProfile.username) {
+        const user: CreateUserDto = {
+          issuer: loggedUser?.iss || '',
+          identity: userProfile.id,
+          username: userProfile.username,
+          lastName: userProfile.lastName || '',
+          firstName: userProfile.firstName || '',
+          email: userProfile.email || '',
+          isAdmin: false
+        };
+        this.backendService.login(user).subscribe(async ok => {
+          console.log('login ok', ok);
+        });
+      }
+    }
     window.addEventListener('message', event => {
       this.appService.processMessagePost(event);
     }, false);
+  }
+
+  logout() {
+    this.authService.logout();
   }
 }
