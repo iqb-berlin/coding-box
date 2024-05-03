96ab8acd2d93589026ee31e3fd4569af0956b3c7
jurei733 <67505990+jurei733@users.noreply.github.com>
2024-05-03 17:18:19 +0200

    Add logo and loading spinner

--- apps/frontend/src/app/services/app.service.ts
+++ apps/frontend/src/app/services/app.service.ts
@@ -1,11 +1,20 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
+import { AppLogoDto } from './app-logo-dto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppService {
+  authData = {
+    userId: 0,
+    userName: '',
+    userLongName: '',
+    isAdmin: false
+  };
+
   dataLoading: boolean | number = false;
+  appLogo: AppLogoDto = standardLogo;
   postMessage$ = new Subject<MessageEvent>();
   processMessagePost(postData: MessageEvent): void {
     const msgData = postData.data;
@@ -15,3 +24,11 @@ export class AppService {
     }
   }
 }
+
+export const standardLogo: AppLogoDto = {
+  data: 'assets/IQB-LogoA.png',
+  alt: 'Zur Startseite',
+  // eslint-disable-next-line max-len
+  bodyBackground: 'linear-gradient(180deg, rgba(7,70,94,1) 0%, rgba(6,112,123,1) 24%, rgba(1,192,229,1) 85%)',
+  boxBackground: 'lightgray'
+};
