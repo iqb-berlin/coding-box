7aa905f8c2f262020e01563221d37f472fdb7df2
jurei733 <67505990+jurei733@users.noreply.github.com>
2024-05-03 15:24:36 +0200

    Add backend users service

---
+++ apps/backend/src/app/database/services/users.service.spec.ts
@@ -0,0 +1,30 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { createMock } from '@golevelup/ts-jest';
+import { Repository } from 'typeorm';
+
+import { UsersService } from './users.service';
+
+import User from '../entities/user.entity';
+
+describe('UsersService', () => {
+  let service: UsersService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: createMock<Repository<User>>()
+        }
+      ]
+    }).compile();
+
+    service = module.get<UsersService>(UsersService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
---
+++ apps/backend/src/app/database/services/users.service.ts
@@ -0,0 +1,198 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { MoreThan, Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import User from '../entities/user.entity';
+import { UserInListDto } from '../../../../../frontend/api-dto/user/user-in-list-dto';
+import { UserFullDto } from '../../../../../frontend/api-dto/user/user-full-dto';
+import { CreateUserDto } from '../../../../../frontend/api-dto/user/create-user-dto';
+import { MyDataDto } from '../../../../../frontend/api-dto/user/my-data-dto';
+
+@Injectable()
+export class UsersService {
+  private readonly logger = new Logger(UsersService.name);
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>
+  ) {
+  }
+
+  async findAllUsers(workspaceId?: number): Promise<UserInListDto[]> {
+    this.logger.log(`Returning users${workspaceId ? ` for workspaceId: ${workspaceId}` : '.'}`);
+    const validUsers: number[] = [];
+    const users: User[] = await this.usersRepository.find({ order: { username: 'ASC' } });
+    const returnUsers: UserInListDto[] = [];
+    users.forEach(user => {
+      if (!workspaceId || (validUsers.indexOf(user.id) > -1)) {
+        const displayName = user.lastName ? user.lastName : user.username;
+        returnUsers.push(<UserInListDto>{
+          id: user.id,
+          name: user.username,
+          isAdmin: user.isAdmin,
+          displayName: user.firstName ? `${displayName}, ${user.firstName}` : displayName
+        });
+      }
+    });
+    return returnUsers;
+  }
+
+  async findOne(id: number): Promise<UserFullDto> {
+    this.logger.log(`Returning user with id: ${id}`);
+    const user = await this.usersRepository.findOne({
+      where: { id: id }
+    });
+    if (user) {
+      return <UserFullDto>{
+        id: user.id,
+        name: user.username,
+        isAdmin: user.isAdmin,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email
+      };
+    }
+    return user;
+  }
+
+  async hasUsers(): Promise<boolean> {
+    this.logger.log('Checking hasUsers');
+    const user = await this.usersRepository.findOne({
+      where: { id: MoreThan(0) },
+      select: { id: true }
+    });
+    this.logger.log(user);
+    return !!user;
+  }
+
+  async create(user: CreateUserDto): Promise<number> {
+    const newUser = this.usersRepository.create(user);
+    await this.usersRepository.save(newUser);
+    return newUser.id;
+  }
+
+  async createUser(user: CreateUserDto): Promise<number> {
+    const existingUser: User = await this.usersRepository.findOne({
+      where: { username: user.username },
+      select: {
+        username: true,
+        id: true,
+        firstName: true,
+        lastName: true,
+        email: true
+      }
+    });
+    const existingKeycloakUser: User = await this.usersRepository.findOne({
+      where: { identity: user.identity, issuer: user.issuer },
+      select: {
+        username: true,
+        id: true,
+        firstName: true,
+        lastName: true,
+        email: true
+      }
+    });
+    if (existingUser) {
+      if (user.username) existingUser.username = user?.username || '';
+      if (user.lastName) existingUser.lastName = user?.lastName || '';
+      if (user.firstName) existingUser.firstName = user?.firstName || '';
+      if (user.email) existingUser.email = user?.email || '';
+      if (user.issuer) existingUser.issuer = user?.issuer;
+      if (user.identity) existingUser.identity = user?.identity;
+      await this.usersRepository.update(
+        { id: existingUser.id },
+        {
+          identity: user.identity,
+          issuer: user.issuer
+        }
+      );
+      this.logger.log(`Updating keycloak user with username: ${JSON.stringify(user)}`);
+      return existingKeycloakUser.id;
+    }
+    if (existingUser) {
+      if (user.username) existingUser.username = user?.username || '';
+      if (user.lastName) existingUser.lastName = user?.lastName || '';
+      if (user.firstName) existingUser.firstName = user?.firstName || '';
+      if (user.email) existingUser.email = user?.email || '';
+      if (user.issuer) existingUser.issuer = user?.issuer;
+      if (user.identity) existingUser.identity = user?.identity;
+      await this.usersRepository.update(
+        { id: existingUser.id },
+        {
+          identity: user.identity,
+          issuer: user.issuer
+        }
+      );
+      this.logger.log(`Updating user with username: ${JSON.stringify(user)}`);
+      return existingUser.id;
+    }
+
+    this.logger.log(`Creating user with username: ${JSON.stringify(user)}`);
+    const newUser = this.usersRepository.create(user);
+    await this.usersRepository.save(newUser);
+    return newUser.id;
+  }
+
+  async getUserIsAdmin(userId: number): Promise<boolean> {
+    const user = await this.usersRepository.findOne({
+      where: { id: userId },
+      select: { isAdmin: true }
+    });
+    if (user) return user.isAdmin;
+    return false;
+  }
+
+  async getLongName(userId: number): Promise<string> {
+    const user = await this.usersRepository.findOne({
+      where: { id: userId },
+      select: { lastName: true, firstName: true, username: true }
+    });
+    if (user) {
+      if (user.lastName) return user.firstName ? `${user.lastName}, ${user.firstName}` : user.lastName;
+      return user.firstName || '';
+    }
+    return '';
+  }
+
+  async remove(id: number | number[]): Promise<void> {
+    this.logger.log(`Deleting user with id: ${id}`);
+    await this.usersRepository.delete(id);
+  }
+
+  async patch(userData: UserFullDto): Promise<void> {
+    this.logger.log(`Updating user with id: ${userData.id}`);
+    const userToUpdate = await this.usersRepository.findOne({
+      where: { id: userData.id },
+      select: {
+        username: true,
+        isAdmin: true,
+        id: true,
+        firstName: true,
+        lastName: true,
+        email: true
+      }
+    });
+    if (userToUpdate) {
+      if (typeof userData.isAdmin === 'boolean') userToUpdate.isAdmin = userData.isAdmin;
+      if (userData.name) userToUpdate.username = userData.name;
+      if (userData.lastName) userToUpdate.lastName = userData.lastName;
+      if (userData.firstName) userToUpdate.firstName = userData.firstName;
+      if (userData.email) userToUpdate.email = userData.email;
+      await this.usersRepository.save(userToUpdate);
+    }
+  }
+
+  async patchMyData(userData: MyDataDto): Promise<void> {
+    const userToUpdate = await this.usersRepository.findOne({
+      where: { id: userData.id },
+      select: {
+        id: true,
+        firstName: true,
+        lastName: true,
+        email: true
+      }
+    });
+    if (userData.lastName) userToUpdate.lastName = userData.lastName;
+    if (userData.firstName) userToUpdate.firstName = userData.firstName;
+    if (userData.email) userToUpdate.email = userData.email;
+    await this.usersRepository.save(userToUpdate);
+  }
+}
