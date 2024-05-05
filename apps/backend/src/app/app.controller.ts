7c985dfbdc54fec6b8498df7492b5ac4b21f981f
jurei733 <67505990+jurei733@users.noreply.github.com>
2024-05-05 20:14:08 +0200

    Remove app service

--- apps/backend/src/app/app.controller.ts
+++ apps/backend/src/app/app.controller.ts
@@ -1,31 +1,23 @@
 import {
   Body,
-  Controller, Get, Post, UploadedFiles, UseInterceptors
+  Controller, Get, Post
 } from '@nestjs/common';
 
 import * as fs from 'fs';
 import * as path from 'path';
-import { FilesInterceptor } from '@nestjs/platform-express';
 import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
-import { AppService } from './app.service';
-import { CreateUserDto } from '../../../frontend/api-dto/user/create-user-dto';
 import { AuthService } from './auth/service/auth.service';
+import { CreateUserDto } from '../../../frontend/api-dto/user/create-user-dto';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService, public authService:AuthService) {}
+  constructor(public authService:AuthService) {}
 
   @Get('player')
   async getPlayer():Promise<string> {
     const fileContent = fs.readFileSync(path.resolve(process.cwd(), 'apps/backend/src/verona/iqb-player-aspect-2.4.1.html'), 'utf8');
     const stringifiedJSON = JSON.stringify(fileContent);
     return stringifiedJSON;
-  }
-
-  @Post('upload/results')
-  @UseInterceptors(FilesInterceptor('files'))
-  async addResultsFiles(@UploadedFiles() files): Promise<any> {
-    return this.appService.uploadResults(files);
   }
 
   @Post('login')
