88ac8c4bd97755a1d3f97163de4d9d24344353c8
jurei733 <67505990+jurei733@users.noreply.github.com>
2024-05-05 20:14:18 +0200

    Add admin routes

--- apps/frontend/src/app/app.routes.ts
+++ apps/frontend/src/app/app.routes.ts
@@ -1,9 +1,20 @@
 import { Routes } from '@angular/router';
 import { ReplayComponent } from './components/replay/replay.component';
 import { HomeComponent } from './components/home/home.component';
+import { AdminComponent } from './components/admin/admin/admin.component';
+import { UsersComponent } from './components/admin/users/users.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
+
   { path: 'home', component: HomeComponent },
   { path: 'replay', component: ReplayComponent },
+  {
+    path: 'admin',
+    component: AdminComponent,
+    children: [
+      { path: '', redirectTo: 'users', pathMatch: 'full' },
+      { path: 'users', component: UsersComponent },
+      { path: '**', component: UsersComponent }]
+  }
 ];
