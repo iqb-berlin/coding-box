c82015474732234ed368d1e8a2a68650c87e7f9d
jurei733 <67505990+jurei733@users.noreply.github.com>
2024-05-06 11:28:08 +0200

    Add admin users backend service

---
+++ apps/backend/src/app/admin/users/users.controller.spec.ts
@@ -0,0 +1,32 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { createMock } from '@golevelup/ts-jest';
+import { UsersController } from './users.controller';
+import { AuthService } from '../../auth/service/auth.service';
+import { UsersService } from '../../database/services/users.service';
+
+describe('UsersController', () => {
+  let controller: UsersController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [UsersController],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: createMock<AuthService>()
+        },
+        {
+          provide: UsersService,
+          useValue: createMock<UsersService>()
+        },
+
+      ]
+    }).compile();
+
+    controller = module.get<UsersController>(UsersController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
---
+++ apps/backend/src/app/admin/users/users.controller.ts
@@ -0,0 +1,64 @@
+import {
+  Body,
+  Controller, Delete, Get, Param, Post
+} from '@nestjs/common';
+import {
+  ApiBearerAuth, ApiCreatedResponse, ApiMethodNotAllowedResponse, ApiOkResponse, ApiQuery, ApiTags
+} from '@nestjs/swagger';
+import { UsersService } from '../../database/services/users.service';
+import { UserFullDto } from '../../../../../frontend/api-dto/user/user-full-dto';
+import { CreateUserDto } from '../../../../../frontend/api-dto/user/create-user-dto';
+
+@Controller('admin/users')
+export class UsersController {
+  constructor(
+    private usersService: UsersService
+  ) {}
+
+  @Get('full')
+  @ApiBearerAuth()
+  @ApiCreatedResponse({
+    type: [UserFullDto]
+  })
+  @ApiTags('admin users')
+  async findAllFull(): Promise<UserFullDto[]> {
+    return this.usersService.findAllFull();
+  }
+
+  // TODO: Delete mit id (statt ids) nur für ein Element (für mehrere s.u.)
+  @Delete(':ids')
+  @ApiBearerAuth()
+  @ApiTags('admin users')
+  @ApiOkResponse({ description: 'Admin users deleted successfully.' })
+  async remove(@Param('ids') ids: string): Promise<void> {
+    const idsAsNumberArray: number[] = [];
+    ids.split(';').forEach(s => idsAsNumberArray.push(parseInt(s, 10)));
+    return this.usersService.remove(idsAsNumberArray);
+  }
+
+  // TODO: Delete mit QueryParam für mehrere Elemente im Frontend implementieren
+  @Delete()
+  @ApiBearerAuth()
+  @ApiTags('admin users')
+  @ApiQuery({
+    name: 'id',
+    type: Number,
+    isArray: true,
+    required: false
+  })
+  @ApiOkResponse({ description: 'Admin users deleted successfully.' })
+  @ApiMethodNotAllowedResponse({ description: 'Active admin user must not be deleted.' })
+  async removeIds(ids: number[]): Promise<void> {
+    return this.usersService.removeIds(ids);
+  }
+  @Post()
+  @ApiBearerAuth()
+  @ApiCreatedResponse({
+    description: 'Sends back the id of the new user in database',
+    type: Number
+  })
+  @ApiTags('admin users')
+  async create(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
+  }
+}
