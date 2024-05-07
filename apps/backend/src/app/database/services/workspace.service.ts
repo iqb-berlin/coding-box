1dad4d0b9b8efe622ed21e93f4cfa3b2ebff8881
jurei733 <67505990+jurei733@users.noreply.github.com>
2024-05-07 18:21:35 +0200

    Find all workspaces from db

--- apps/backend/src/app/database/services/workspace.service.ts
+++ apps/backend/src/app/database/services/workspace.service.ts
@@ -1,14 +1,12 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { ArgumentOutOfRangeError } from 'rxjs';
 import Workspace from '../entities/workspace.entity';
 import { WorkspaceInListDto } from '../../../../../frontend/api-dto/workspaces/workspace-in-list-dto';
 import { WorkspaceFullDto } from '../../../../../frontend/api-dto/workspaces/workspace-full-dto';
 import { CreateWorkspaceDto } from '../../../../../frontend/api-dto/workspaces/create-workspace-dto';
 import { AdminWorkspaceNotFoundException } from '../../exceptions/admin-workspace-not-found.exception';
-import WorkspaceAdmin from '../entities/workspace-admin.entity';
-import { UsersService } from './users.service';
+import { FileIo } from '../../admin/test-files/interfaces/file-io.interface';
 
 @Injectable()
 export class WorkspaceService {
@@ -16,29 +14,14 @@ export class WorkspaceService {
 
   constructor(
     @InjectRepository(Workspace)
-    private workspaceRepository: Repository<Workspace>,
-    @InjectRepository(WorkspaceAdmin)
-    private workspaceAdminRepository: Repository<WorkspaceAdmin>,
-    private usersService: UsersService
-
-  ) {}
+    private workspaceRepository: Repository<Workspace>
+  ) {
+  }
 
   async findAll(userId?: number): Promise<WorkspaceInListDto[]> {
     this.logger.log('Returning all workspace groups.');
-    let usersWorkspaceGroupIds: number[] = [];
-    if (userId) {
-      const usersWorkspaceGroups = await this.workspaceAdminRepository.find({
-        where: { userId: userId }
-      });
-      if (usersWorkspaceGroups && usersWorkspaceGroups.length > 0) {
-        usersWorkspaceGroupIds = usersWorkspaceGroups.map(wsg => wsg.workspaceGroupId);
-      } else {
-        return [];
-      }
-    }
-
-    const returnWorkspaces: WorkspaceInListDto[] = [];
-    return returnWorkspaces;
+    const workspaces = await this.workspaceRepository.find({});
+    return workspaces.map(workspace => ({ id: workspace.id, name: workspace.name }));
   }
 
   async findOne(id: number): Promise<WorkspaceFullDto> {
@@ -57,26 +40,26 @@ export class WorkspaceService {
     throw new AdminWorkspaceNotFoundException(id, 'GET');
   }
 
-  async create(workspaceGroup: CreateWorkspaceDto): Promise<number> {
-    this.logger.log(`Creating workspace with name: ${workspaceGroup.name}`);
-    const newWorkspaceGroup = this.workspaceRepository.create(workspaceGroup);
-    await this.workspaceRepository.save(newWorkspaceGroup);
-    return newWorkspaceGroup.id;
+  async create(workspace: CreateWorkspaceDto): Promise<number> {
+    this.logger.log(`Creating workspace with name: ${workspace.name}`);
+    const newWorkspace = this.workspaceRepository.create(workspace);
+    await this.workspaceRepository.save(newWorkspace);
+    return newWorkspace.id;
   }
 
-  async patch(workspaceGroupData: WorkspaceFullDto): Promise<void> {
-    this.logger.log(`Updating workspace with id: ${workspaceGroupData.id}`);
-    if (workspaceGroupData.id) {
-      const workspaceGroupToUpdate = await this.workspaceRepository.findOne({
-        where: { id: workspaceGroupData.id }
-      });
-      if (workspaceGroupData.name) workspaceGroupToUpdate.name = workspaceGroupData.name;
-      if (workspaceGroupData.settings) workspaceGroupToUpdate.settings = workspaceGroupData.settings;
-      await this.workspaceRepository.save(workspaceGroupToUpdate);
-    } else {
-      throw new ArgumentOutOfRangeError();
-    }
-  }
+  // async patch(workspaceGroupData: WorkspaceFullDto): Promise<void> {
+  //   this.logger.log(`Updating workspace with id: ${workspaceGroupData.id}`);
+  //   if (workspaceGroupData.id) {
+  //     const workspaceGroupToUpdate = await this.workspaceRepository.findOne({
+  //       where: { id: workspaceGroupData.id }
+  //     });
+  //     if (workspaceGroupData.name) workspaceGroupToUpdate.name = workspaceGroupData.name;
+  //     if (workspaceGroupData.settings) workspaceGroupToUpdate.settings = workspaceGroupData.settings;
+  //     await this.workspaceRepository.save(workspaceGroupToUpdate);
+  //   } else {
+  //     throw new ArgumentOutOfRangeError();
+  //   }
+  // }
 
   async remove(id: number[]): Promise<void> {
     this.logger.log(`Deleting workspaces with ids: ${id.join(', ')}`);
