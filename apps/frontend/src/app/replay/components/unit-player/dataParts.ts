9c34768cfc8b8a2633121830e87ddf31b238300a
jurei733 <67505990+jurei733@users.noreply.github.com>
2024-05-22 16:15:09 +0200

    Remove unused example dataparts

--- apps/frontend/src/app/replay/components/unit-player/dataParts.ts
+++
@@ -1,4 +0,0 @@
-export const dataParts = {
-  stateVariableCodes: '[]',
-  elementCodes: '[{"id":"radio_1","value":3,"status":"VALUE_CHANGED"},{"id":"text_2","value":[],"status":"DISPLAYED"},{"id":"text_31","value":[],"status":"DISPLAYED"},{"id":"button_2","value":null,"status":"DISPLAYED"}]'
-}
