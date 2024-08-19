9576ccbfeb59b21848ddc71424cfc4a3aeb87ff1
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2024-08-19 10:55:56 +0200

    Raise version to 0.2.7

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -25,7 +25,7 @@ async function bootstrap() {
     const config = new DocumentBuilder()
       .setTitle('IQB Coding Box API')
       .setDescription('The Coding Box API description and try-out')
-      .setVersion('0.2.6')
+      .setVersion('0.2.7')
       .addBearerAuth()
       .build();
     const document = SwaggerModule.createDocument(app, config);
