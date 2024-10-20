fbedb43605f936a2cabcf15e57c47f9de3579690
jurei733 <67505990+jurei733@users.noreply.github.com>
2024-10-20 16:18:26 +0200

    Add test results explicit button

--- apps/frontend/src/app/ws-admin/components/test-groups/test-groups.component.ts
+++ apps/frontend/src/app/ws-admin/components/test-groups/test-groups.component.ts
@@ -17,27 +17,31 @@ import {
 import { MatSort, MatSortHeader } from '@angular/material/sort';
 import { FormsModule } from '@angular/forms';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
-import { MatAnchor } from '@angular/material/button';
+import { MatTooltip } from '@angular/material/tooltip';
+import { MatAnchor, MatButton } from '@angular/material/button';
 import { MatCheckbox } from '@angular/material/checkbox';
 import { SelectionModel } from '@angular/cdk/collections';
-import { DatePipe } from '@angular/common';
+import { DatePipe, JsonPipe } from '@angular/common';
 import { MatProgressSpinner } from '@angular/material/progress-spinner';
 import { MatIcon } from '@angular/material/icon';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { BackendService } from '../../../services/backend.service';
 import { AppService } from '../../../services/app.service';
+import { WrappedIconComponent } from '../../../shared/wrapped-icon/wrapped-icon.component';
 import { SearchFilterComponent } from '../../../shared/search-filter/search-filter.component';
 import { HasSelectionValuePipe } from '../../../shared/pipes/hasSelectionValue.pipe';
 import { IsAllSelectedPipe } from '../../../shared/pipes/isAllSelected.pipe';
 import { IsSelectedPipe } from '../../../shared/pipes/isSelected.pipe';
+import { FileSizePipe } from '../../../shared/pipes/filesize.pipe';
 import { TestGroupsInListDto } from '../../../../../../../api-dto/test-groups/testgroups-in-list.dto';
 
 @Component({
   selector: 'coding-box-test-groups',
   templateUrl: './test-groups.component.html',
   styleUrls: ['./test-groups.component.scss'],
+  standalone: true,
   // eslint-disable-next-line max-len
-  imports: [MatTable, MatSort, MatColumnDef, MatHeaderCellDef, MatHeaderCell, MatCheckbox, MatCellDef, MatCell, MatSortHeader, MatHeaderRowDef, MatHeaderRow, MatRowDef, MatRow, FormsModule, TranslateModule, SearchFilterComponent, HasSelectionValuePipe, IsAllSelectedPipe, IsSelectedPipe, MatProgressSpinner, DatePipe, MatAnchor, MatIcon]
+  imports: [MatTable, MatSort, MatColumnDef, MatHeaderCellDef, MatHeaderCell, MatCheckbox, MatCellDef, MatCell, MatSortHeader, MatHeaderRowDef, MatHeaderRow, MatRowDef, MatRow, MatButton, MatTooltip, WrappedIconComponent, FormsModule, TranslateModule, SearchFilterComponent, JsonPipe, HasSelectionValuePipe, IsAllSelectedPipe, IsSelectedPipe, MatProgressSpinner, DatePipe, FileSizePipe, MatAnchor, MatIcon]
 })
 export class TestGroupsComponent implements OnInit {
   displayedColumns = ['selectCheckbox', 'test_group', 'created_at'];
@@ -86,6 +90,31 @@ export class TestGroupsComponent implements OnInit {
           this.isLoading = false;
         }
       });
+  }
+
+  onFileSelected(targetElement: EventTarget | null) {
+    if (targetElement) {
+      const inputElement = targetElement as HTMLInputElement;
+      if (inputElement.files && inputElement.files.length > 0) {
+        this.isLoading = true;
+        this.backendService.uploadTestFiles(
+          this.appService.selectedWorkspaceId,
+          inputElement.files
+        ).subscribe(() => {
+          setTimeout(() => {
+            this.createTestGroupsList(true);
+          }, 1000);
+          this.isLoading = false;
+        });
+      }
+    }
+  }
+
+  createCodingTestGroups():void {
+    this.isLoading = true;
+    const selectedTestGroups = this.tableSelectionCheckboxes.selected;
+    this.backendService.createCodingTestGroups(selectedTestGroups).subscribe(() => {
+    });
   }
 
   masterToggle(): void {
