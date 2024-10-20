e3476197da88913e9ca31c89020856c091c62472
jurei733 <67505990+jurei733@users.noreply.github.com>
2024-10-20 16:19:19 +0200

    Transform results into tc_merge format

--- database/changelog/coding-box.changelog-0.3.0.sql
+++ database/changelog/coding-box.changelog-0.3.0.sql
@@ -13,3 +13,16 @@ CREATE TABLE "public"."logs"
   "booklet_id"   VARCHAR(100)
 );
 -- rollback DROP TABLE "public"."logs";
+
+-- changeset jurei733:2
+CREATE TABLE public.persons (
+  "id"            SERIAL PRIMARY KEY,
+  "group"         VARCHAR(100) NOT NULL,
+  "login"         VARCHAR(100) NOT NULL,
+  "code"          VARCHAR(100) NOT NULL,
+  "booklets"      JSON DEFAULT [] NOT NULL,
+  "workspace_id"  INTEGER ,
+  "uploaded_at"   TIMESTAMP WITH TIME ZONE DEFAULT now(),
+  "source"        VARCHAR(100)
+);
+-- rollback DROP TABLE "public"."persons";
