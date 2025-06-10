c837f751ad68a038ea780924029d43bf1f51ab37
jurei733 <67505990+jurei733@users.noreply.github.com>
2025-06-10 13:03:49 +0200

    Fix unit tests

--- apps/frontend/src/app/sys-admin/components/sys-admin-settings/sys-admin-settings.component.spec.ts
+++ apps/frontend/src/app/sys-admin/components/sys-admin-settings/sys-admin-settings.component.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { TranslateModule } from '@ngx-translate/core';
+import { provideHttpClient } from '@angular/common/http';
+import { provideNoopAnimations } from '@angular/platform-browser/animations'; // Importieren
 import { SysAdminSettingsComponent } from './sys-admin-settings.component';
 import { environment } from '../../../../environments/environment';
 
@@ -9,17 +11,18 @@ describe('SysAdminSettingsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-
-      providers: [{
-        provide: 'SERVER_URL',
-        useValue: environment.backendUrl
-      }],
+      providers: [
+        provideHttpClient(),
+        {
+          provide: 'SERVER_URL',
+          useValue: environment.backendUrl
+        },
+        provideNoopAnimations() // Hier hinzufügen
+      ],
       imports: [
-
         TranslateModule.forRoot()
       ]
     }).compileComponents();
-
     fixture = TestBed.createComponent(SysAdminSettingsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
