5e165452f66c71486914d2b5797c7b143576108a
jurei733 <67505990+jurei733@users.noreply.github.com>
2025-07-23 12:57:23 +0200

    Improve the login button look

--- apps/frontend/src/app/workspace/components/user-workspaces/user-workspaces.component.ts
+++ apps/frontend/src/app/workspace/components/user-workspaces/user-workspaces.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, inject } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { RouterLink } from '@angular/router';
 import { MatAnchor, MatButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
 import { WorkspaceFullDto } from '../../../../../../../api-dto/workspaces/workspace-full-dto';
 import { AuthService } from '../../../core/services/auth.service';
 
@@ -9,7 +10,7 @@ import { AuthService } from '../../../core/services/auth.service';
   selector: 'coding-book-user-workspaces',
   templateUrl: './user-workspaces.component.html',
   styleUrls: ['./user-workspaces.component.scss'],
-  imports: [MatAnchor, RouterLink, TranslateModule, MatButton]
+  imports: [MatAnchor, RouterLink, TranslateModule, MatButton, MatIcon]
 })
 
 export class UserWorkspacesComponent {
