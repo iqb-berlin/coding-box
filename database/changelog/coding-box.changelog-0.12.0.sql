044d02a5f1fd026f79521e692798c12d9f9c99e8
jurei733 <67505990+jurei733@users.noreply.github.com>
2025-08-04 02:14:45 +0200

    Fix changelog 0.12.

--- database/changelog/coding-box.changelog-0.12.0.sql
+++ database/changelog/coding-box.changelog-0.12.0.sql
@@ -2,9 +2,9 @@
 
 -- changeset jurei733:1
 CREATE TABLE IF NOT EXISTS "public"."coding_job" (
-  "id" INTEGER PRIMARY KEY REFERENCES "public"."job"("id") ON DELETE CASCADE,
-  "name" VARCHAR(255) NOT NULL,
-  "description" TEXT NULL
+                                                   "id" INTEGER PRIMARY KEY REFERENCES "public"."job"("id") ON DELETE CASCADE,
+                                                   "name" VARCHAR(255) NOT NULL,
+                                                   "description" TEXT NULL
 );
 -- rollback DROP TABLE IF EXISTS "public"."coding_job";
 
@@ -14,57 +14,57 @@ CREATE INDEX IF NOT EXISTS "idx_coding_job_name" ON "public"."coding_job"("name"
 
 -- changeset jurei733:3
 CREATE TABLE IF NOT EXISTS "public"."variable" (
-  "id" SERIAL PRIMARY KEY,
-  "workspace_id" INTEGER NOT NULL,
-  "unit_name" VARCHAR(255) NOT NULL,
-  "variable_id" VARCHAR(255) NOT NULL
+                                                 "id" SERIAL PRIMARY KEY,
+                                                 "workspace_id" INTEGER NOT NULL,
+                                                 "unit_name" VARCHAR(255) NOT NULL,
+                                                 "variable_id" VARCHAR(255) NOT NULL
 );
 -- rollback DROP TABLE IF EXISTS "public"."variable";
 
 -- changeset jurei733:4
 CREATE TABLE IF NOT EXISTS "public"."variable_bundle" (
-  "id" SERIAL PRIMARY KEY,
-  "workspace_id" INTEGER NOT NULL,
-  "name" VARCHAR(255) NOT NULL,
-  "description" TEXT NULL,
-  "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  "updated_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+                                                        "id" SERIAL PRIMARY KEY,
+                                                        "workspace_id" INTEGER NOT NULL,
+                                                        "name" VARCHAR(255) NOT NULL,
+                                                        "description" TEXT NULL,
+                                                        "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                                                        "updated_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 -- rollback DROP TABLE IF EXISTS "public"."variable_bundle";
 
 -- changeset jurei733:5
 CREATE TABLE IF NOT EXISTS "public"."coding_job_variable" (
-  "coding_job_id" INTEGER NOT NULL REFERENCES "public"."coding_job"("id") ON DELETE CASCADE,
-  "variable_id" INTEGER NOT NULL REFERENCES "public"."variable"("id") ON DELETE CASCADE,
-  PRIMARY KEY ("coding_job_id", "variable_id")
+                                                            "coding_job_id" INTEGER NOT NULL REFERENCES "public"."coding_job"("id") ON DELETE CASCADE,
+                                                            "variable_id" INTEGER NOT NULL REFERENCES "public"."variable"("id") ON DELETE CASCADE,
+                                                            PRIMARY KEY ("coding_job_id", "variable_id")
 );
 -- rollback DROP TABLE IF EXISTS "public"."coding_job_variable";
 
 -- changeset jurei733:6
 CREATE TABLE IF NOT EXISTS "public"."coding_job_variable_bundle" (
-  "coding_job_id" INTEGER NOT NULL REFERENCES "public"."coding_job"("id") ON DELETE CASCADE,
-  "variable_bundle_id" INTEGER NOT NULL REFERENCES "public"."variable_bundle"("id") ON DELETE CASCADE,
-  PRIMARY KEY ("coding_job_id", "variable_bundle_id")
+                                                                   "coding_job_id" INTEGER NOT NULL REFERENCES "public"."coding_job"("id") ON DELETE CASCADE,
+                                                                   "variable_bundle_id" INTEGER NOT NULL REFERENCES "public"."variable_bundle"("id") ON DELETE CASCADE,
+                                                                   PRIMARY KEY ("coding_job_id", "variable_bundle_id")
 );
 -- rollback DROP TABLE IF EXISTS "public"."coding_job_variable_bundle";
 
 -- changeset jurei733:7
 CREATE TABLE IF NOT EXISTS "public"."variable_bundle_variables" (
-  "bundle_id" INTEGER NOT NULL REFERENCES "public"."variable_bundle"("id") ON DELETE CASCADE,
-  "variable_bundle_id" INTEGER NOT NULL REFERENCES "public"."variable"("id") ON DELETE CASCADE,
-  PRIMARY KEY ("bundle_id", "variable_bundle_id")
+                                                                  "bundle_id" INTEGER NOT NULL REFERENCES "public"."variable_bundle"("id") ON DELETE CASCADE,
+                                                                  "variable_bundle_id" INTEGER NOT NULL REFERENCES "public"."variable"("id") ON DELETE CASCADE,
+                                                                  PRIMARY KEY ("bundle_id", "variable_bundle_id")
 );
 -- rollback DROP TABLE IF EXISTS "public"."variable_bundle_variables";
 
 -- changeset jurei733:8
 CREATE TABLE IF NOT EXISTS "public"."coding_job_coders" (
-  "coding_job_id" INTEGER NOT NULL REFERENCES "public"."coding_job"("id") ON DELETE CASCADE,
-  "coder_id" INTEGER NOT NULL,
-  PRIMARY KEY ("coding_job_id", "coder_id")
+                                                          "coding_job_id" INTEGER NOT NULL REFERENCES "public"."coding_job"("id") ON DELETE CASCADE,
+                                                          "coder_id" INTEGER NOT NULL,
+                                                          PRIMARY KEY ("coding_job_id", "coder_id")
 );
 -- rollback DROP TABLE IF EXISTS "public"."coding_job_coders";
 
 -- changeset jurei733:9
 ALTER TABLE "public"."variable_bundle_variables"
-RENAME COLUMN "variable_bundle_id" TO "variable_id";
+  RENAME COLUMN "variable_bundle_id" TO "variable_id";
 -- rollback ALTER TABLE "public"."variable_bundle_variables" RENAME COLUMN "variable_id" TO "variable_bundle_id";
