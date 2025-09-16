ee177c798224191aa0c5608da5108dfffb0f251b
jurei733 <67505990+jurei733@users.noreply.github.com>
2025-09-16 16:36:16 +0200

    Fix workspace_id validation  by updating the @WorkspaceId() decorator to ensure it is a positive number.

--- apps/backend/src/app/admin/workspace/workspace.decorator.ts
+++ apps/backend/src/app/admin/workspace/workspace.decorator.ts
@@ -1,9 +1,15 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { createParamDecorator, ExecutionContext, BadRequestException } from '@nestjs/common';
 
 export const WorkspaceId = createParamDecorator(
   (data: unknown, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
     const params = request.params;
-    return params.workspace_id;
+    const workspaceId = parseInt(params.workspace_id, 10);
+
+    if (Number.isNaN(workspaceId) || workspaceId <= 0) {
+      throw new BadRequestException('workspace_id must be a positive number');
+    }
+
+    return workspaceId;
   }
 );
