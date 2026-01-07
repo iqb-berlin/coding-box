293b24d2e8ce955d613e67a0596b6eeabbea4e27
jurei733 <67505990+jurei733@users.noreply.github.com>
2026-01-07 10:06:34 +0100

    Remove `DatabaseModule` and move database connection setup to `AppModule`
    - Eliminated `DatabaseModule` to simplify module structure.
    - Integrated database connection configuration directly into `AppModule`.
    - Retained `autoLoadEntities` and `synchronize` settings for seamless entity management.

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { HttpModule } from '@nestjs/axios';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
-import { DatabaseModule } from './database/database.module';
 import { AdminModule } from './admin/admin.module';
 import { UsersModule } from './users/users.module';
 import { WorkspacesModule } from './workspaces/workspaces.module';
@@ -14,11 +14,37 @@ import { CacheModule } from './cache/cache.module';
 import { WsgAdminModule } from './wsg-admin/wsg-admin.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    isGlobal: true,
-    envFilePath: '.env.dev',
-    cache: true
-  }), AuthModule, DatabaseModule, UsersModule, WorkspacesModule, CodingModule, AdminModule, HttpModule, JobQueueModule, HealthModule, CacheModule, WsgAdminModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env.dev',
+      cache: true
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('POSTGRES_HOST'),
+        port: +configService.get<number>('POSTGRES_PORT'),
+        username: configService.get('POSTGRES_USER'),
+        password: configService.get('POSTGRES_PASSWORD'),
+        database: configService.get('POSTGRES_DB'),
+        autoLoadEntities: true,
+        synchronize: false
+      }),
+      inject: [ConfigService]
+    }),
+    AuthModule,
+    UsersModule,
+    WorkspacesModule,
+    CodingModule,
+    AdminModule,
+    HttpModule,
+    JobQueueModule,
+    HealthModule,
+    CacheModule,
+    WsgAdminModule
+  ],
   controllers: [AppController]
 })
 export class AppModule {}
