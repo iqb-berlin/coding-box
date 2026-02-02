eba6c6b87c297b84104d226d651f3bd151057641
svwolter <sven.wolter@iqb.hu-berlin.de>
2026-02-02 16:40:49 +0100

    Rename 'KEYCLOAK_URL' to 'OIDC_PROVIDER_URL'

--- apps/backend/src/app/auth/auth.module.ts
+++ apps/backend/src/app/auth/auth.module.ts
@@ -3,7 +3,7 @@ import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { AuthService } from './service/auth.service';
 import { OAuth2ClientCredentialsService } from './service/oauth2-client-credentials.service';
-import { KeycloakAuthService } from './service/keycloak-auth.service';
+import { OidcAuthService } from './service/oidc-auth.service';
 import { AuthController } from './auth.controller';
 import { DatabaseModule } from '../database/database.module';
 import { UserModule } from '../user/user.module';
@@ -18,7 +18,7 @@ import { JwtStrategy } from './jwt.strategy';
     HttpModule
   ],
   controllers: [AuthController],
-  providers: [AuthService, OAuth2ClientCredentialsService, KeycloakAuthService, JwtStrategy],
-  exports: [AuthService, OAuth2ClientCredentialsService, KeycloakAuthService]
+  providers: [AuthService, OAuth2ClientCredentialsService, OidcAuthService, JwtStrategy],
+  exports: [AuthService, OAuth2ClientCredentialsService, OidcAuthService]
 })
 export class AuthModule { }
