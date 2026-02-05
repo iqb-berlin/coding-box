424f5b01bd5d8d34cffef2c6e3a608ba97be03ef
svwolter <sven.wolter@iqb.hu-berlin.de>
2026-02-05 18:08:58 +0100

    Delete obsolete docker compose environment variable 'API_HOST'

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -9,7 +9,7 @@ import { AppModule } from './app/app.module';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const configService = app.get(ConfigService);
-  const host = configService.get('API_HOST') || 'localhost';
+  const host = configService.get('SERVER_NAME') !== null && configService.get('SERVER_NAME') !== undefined && configService.get('SERVER_NAME') !== 'localhost' ? 'backend' : 'localhost';
   const port = 3333;
   const globalPrefix = 'api';
 
